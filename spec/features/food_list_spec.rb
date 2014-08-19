aa5a05c47caddf55c711efedea6a38fb22b52013
Alex Ludlow <alexludlow@aubreys-air-8.gateway.pace.com>
2014-08-18 22:25:28 -0700

    Added Title to list page

--- spec/features/food_list_spec.rb
+++ spec/features/food_list_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 describe "Food lists" do
   describe "The very high oxalate food list" do
     it "lists very high oxalate foods" do
-      list = List.create(foods: ["spinach"])
+      list = List.create(name: "Very High Oxalate Foods", foods: ["spinach"])
 
       visit list_path(list)
-
+      expect(page).to have_content("Very High Oxalate Foods")
       expect(page).to have_content(/spinach/i)
     end
   end
 
   describe "The high oxalate food list" do
     it "lists high oxalate foods" do
-      list = List.create(foods: ["chocolate"])
+      list = List.create(name: "High Oxalate Foods", foods: ["chocolate"])
 
       visit list_path(list)
-
+      expect(page).to have_content("High Oxalate Foods")
       expect(page).to have_content(/chocolate/i)
     end
   end
