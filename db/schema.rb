d4d91a222081099d21c5a646154dd911ce0bbcf9
Adam Stegman <astegman@gmail.com>
2014-10-14 22:04:01 -0700

    Switch from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140820061811) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "foods", force: true do |t|
     t.string   "name"
     t.integer  "list_id"
