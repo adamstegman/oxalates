f2c8459454a485f97e129dd5db894a262fc9fed0
Adam Stegman <astegman@gmail.com>
2014-10-20 19:27:39 -0700

    Fix whitespace

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,17 +1,17 @@
 class FoodsController < ApplicationController
   def new
-  	@food = Food.new
+    @food = Food.new
   end
 
   def create
-  	list = List.find(params[:food][:list])
-  	food_params = params.require(:food).permit(:name)
-  	list.foods << Food.new(food_params)
-  	redirect_to list
+    list = List.find(params[:food][:list])
+    food_params = params.require(:food).permit(:name)
+    list.foods << Food.new(food_params)
+    redirect_to list
   end
 
   def search
-  	# SELECT * from foods WHERE `foods`.name LIKE "%chocolate%"
-  	@results = Food.where('name LIKE ?', "%#{params[:search]}%")
+    # SELECT * from foods WHERE `foods`.name LIKE "%chocolate%"
+    @results = Food.where('name LIKE ?', "%#{params[:search]}%")
   end
 end
--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
 describe "searching for food" do
-    before(:all) do
-        List.find_or_create_by(name: "High Oxalate Foods") do |list|
-            list.foods << Food.new(name: "chocolate")
-		end
-		List.find_or_create_by(name: "Moderate Oxalate Foods") do |list|
-  			list.foods << Food.new(name: "hot chocolate")
-    	end
+  before(:all) do
+    List.find_or_create_by(name: "High Oxalate Foods") do |list|
+      list.foods << Food.new(name: "chocolate")
     end
-    after(:all) do
-        List.destroy_all
+    List.find_or_create_by(name: "Moderate Oxalate Foods") do |list|
+      list.foods << Food.new(name: "hot chocolate")
     end
+  end
+  after(:all) do
+    List.destroy_all
+  end
 
-	it "displays matching foods' lists" do
-		visit "/"
-		fill_in "Search", with: "chocolate"
-		click_on "Search"
-		expect(page).to have_content("chocolate")
-		expect(page).to have_content("hot chocolate")
-		expect(page).to have_content("High Oxalate")
-		expect(page).to have_content("Moderate Oxalate")
-	end
+  it "displays matching foods' lists" do
+    visit "/"
+    fill_in "Search", with: "chocolate"
+    click_on "Search"
+    expect(page).to have_content("chocolate")
+    expect(page).to have_content("hot chocolate")
+    expect(page).to have_content("High Oxalate")
+    expect(page).to have_content("Moderate Oxalate")
+  end
 end
