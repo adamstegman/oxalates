127cc82a9f97d6bff41db918315fde906cb8c3e7
Adam Stegman <astegman@gmail.com>
2014-11-15 10:59:37 -0800

    Color food name based on list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def list_class_name(list)
+    list_class = list.name.sub(/ Oxalate Foods\z/, '').downcase.gsub(/\W+/, '-').sub(/\A-/, '').sub(/-\z/, '')
+    "list--#{list_class}"
+  end
 end
