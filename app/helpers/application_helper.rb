40d8d5b223809a218b65648430ee53b5027048e0
Adam Stegman <astegman@gmail.com>
2014-11-28 23:54:45 -0800

    Add list menu item icon styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,19 @@ module ApplicationHelper
   def list_class_name(list)
     list.name.sub(/ Oxalate Foods\z/, '').downcase.gsub(/\W+/, '-').sub(/\A-/, '').sub(/-\z/, '')
   end
+
+  def list_menu_item_icon_line_colors(list)
+    case list.name
+    when 'Very High'
+      %w(black) * 4
+    when 'High'
+      %w(#e00) * 4
+    when 'Moderate'
+      %w(#fa0) * 4
+    when 'Low'
+      %w(green) * 4
+    else
+      %w(black #e00 #fa0 green)
+    end
+  end
 end
