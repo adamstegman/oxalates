7c62592d84705b8bde131d0f01f48c7136947c22
Adam Stegman <astegman@onemedical.com>
2015-05-23 20:19:02 -0700

    Make the All Foods list the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'lists#index'
+  root 'lists#show', id: AllFoodsList::ID
   resources :lists, only: [:show, :index, :edit, :update]
 
   resources :foods, only: [:new, :create, :edit, :update, :destroy]
