5a2f2378d2bc2ed489daa3fe4073ec33d81c9c69
Adam Stegman <me@adamstegman.com>
2018-10-06 11:35:00 -0500

    Search on text entry

--- app/javascript/oxalates/SearchForm.jsx
+++ app/javascript/oxalates/SearchForm.jsx
@@ -3,14 +3,9 @@ import React from 'react';
 import './SearchForm.scss';
 
 export class SearchForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.query = React.createRef();
-  }
-
   onSubmit(event) {
     event.preventDefault();
-    this._onSubmit(this.query.current.value);
+    this._onSubmit(event.target.value);
   }
 
   render() {
@@ -20,10 +15,11 @@ export class SearchForm extends React.Component {
     } = this.props;
     this._onSubmit = onSubmit;
 
+    const onChange = this.onSubmit.bind(this);
     return (
-      <form className="search" onSubmit={event => this.onSubmit(event)}>
+      <form className="search" onSubmit={onChange}>
         <label htmlFor="search">Search</label>
-        <input type="search" name="search" placeholder="Search" ref={this.query} defaultValue={query} />
+        <input type="search" name="search" placeholder="Search" value={query} onChange={onChange} />
         <input type="submit" value="Search" />
       </form>
     );
